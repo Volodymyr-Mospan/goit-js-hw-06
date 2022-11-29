5e1235485cdea8ede7f44e5fb10a5cb9ccab8c9b
Volodymyr <mospan.volodymyr@gmail.com>
2022-11-29 15:40:37 +0200

    adds completed task 4

---
+++ js/task-04.js
@@ -0,0 +1,10 @@
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const valueRef = document.querySelector('#value');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+decrementBtn.addEventListener('click', () => (valueRef.textContent -= 1));
+
+incrementBtn.addEventListener(
+  'click',
+  () => (valueRef.textContent = Number(valueRef.textContent) + 1)
+);

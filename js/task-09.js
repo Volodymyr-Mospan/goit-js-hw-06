e9a35a01b35a11268fe27a91af52b0e84c83188b
Volodymyr <mospan.volodymyr@gmail.com>
2022-12-02 21:23:54 +0200

    adds completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const buttonRef = document.querySelector('button.change-color');
+const spanColorRef = document.querySelector('span.color');
+
+buttonRef.addEventListener('click', onButtonChangeColor);
+
+function onButtonChangeColor() {
+  const backgroundColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = `${backgroundColor}`;
+  spanColorRef.textContent = backgroundColor;
+}
